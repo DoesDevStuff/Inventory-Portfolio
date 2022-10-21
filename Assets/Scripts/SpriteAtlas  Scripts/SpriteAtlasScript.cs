4eae4c11b6bd2afdbd56f403aa338f73381b20bc
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-10-21 20:12:41 +0100

    Sprite Atlas Scene redone
    Enabled Sprite atlas in Project Settings
    Re-added the Sprites and Atlas assets
    As well as the scripts in their respective folders

---
+++ Assets/Scripts/SpriteAtlas  Scripts/SpriteAtlasScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.U2D;
+
+
+public class SpriteAtlasScript : MonoBehaviour
+{
+    [SerializeField] private SpriteAtlas _mainAtlas;
+    [SerializeField] private string _individualSprite_name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().sprite = _mainAtlas.GetSprite(_individualSprite_name);
+    }
+}
