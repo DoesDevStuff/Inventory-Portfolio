fe8c9502a7f6c6b4c5ed75011454748806fd61b2
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-11-16 16:57:13 +0000

    Added background game stuff + animations and health bar
    Working player with animations and healthbar
    This is so that we can eventually test the pick up system as well

---
+++ Assets/Scripts/InitialStarterElements/Player/Health.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 10;
+    [SerializeField] private FloatValueSO currentHealth;
+
+    //[SerializeField] private GameObject bloodParticle;
+
+    //[SerializeField] private Renderer renderer;
+    [SerializeField] private float flashTime = 0.2f;
+
+    [Space]
+    [Header("References:")]
+    public Animator animator;
+
+    private void Start()
+    {
+        currentHealth.Value = 1;
+    }
+
+    public void Reduce(int damage)
+    {
+        currentHealth.Value -= damage / maxHealth;
+
+        CreateHitFeedback();
+        if (currentHealth.Value <= 0)
+        {
+            animator.SetTrigger("Death");
+            Die();
+            
+        }
+    }
+
+    public void AddHealth(int healthBoost)
+    {
+        int health = Mathf.RoundToInt(currentHealth.Value * maxHealth);
+        int val = health + healthBoost;
+        currentHealth.Value = (val > maxHealth ? maxHealth : val / maxHealth);
+    }
+
+    private void CreateHitFeedback()
+    {
+        animator.SetTrigger("Hit");
+        //Instantiate(bloodParticle, transform.position, Quaternion.identity);
+        //StartCoroutine(FlashFeedback());
+    }
+
+    /*
+    private IEnumerator FlashFeedback()
+    {
+        renderer.material.SetInt("_Flash", 1);
+        yield return new WaitForSeconds(flashTime);
+        renderer.material.SetInt("_Flash", 0);
+    }
+    */
+
+    private void Die()
+    {
+        Debug.Log("Died");
+        currentHealth.Value = 1;
+    }
+}
+
